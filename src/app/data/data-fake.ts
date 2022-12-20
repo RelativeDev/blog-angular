09c13362bf358927ba0dfa8c69787b53d57b6fa7
Pedro Gomes <pedrogomespeixoto14@gmail.com>
2022-12-20 19:27:37 -0200

    content created

---
+++ src/app/data/data-fake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        'id': '1',
+        'title': 'Novo Computador Quântico',
+        'description': 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.',
+        'photoCover': 'https://t.ctcdn.com.br/rTF4uJmxpEpltLkb5ObpssDZzug=/512x288/smart/filters:format(webp)/i519239.png'
+    },
+    {
+        'id': '2',
+        'title': 'The standard Lorem Ipsum passage',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
+        'photoCover': 'https://bernardmarr.com/wp-content/uploads/2021/08/The-Future-of-Quantum-Computing.jpg'
+    }
+]
